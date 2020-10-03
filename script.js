a76bbc19c2f041877f2861ffe44ca4f54daee944
Mateusz Choma <kontakt@coderoad.pl>
2020-10-03 08:47:00 +0200

    Simplest impementation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const orderTotal = (cart) => {}
+const orderTotal = (cart) => {
+    let sum = 0
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i]
+        sum = sum + product.price
+    }
+    return sum
+}
