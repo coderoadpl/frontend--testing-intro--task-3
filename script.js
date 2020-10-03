99b2a915051761093358026c7bf8df6197d9ba6b
Mateusz Choma <kontakt@coderoad.pl>
2020-10-03 08:49:59 +0200

    orderTotal refactored

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const orderTotal = (cart) => {
-    let sum = 0
-    for (let i = 0; i < cart.length; i++) {
-        const product = cart[i]
-        sum = sum + product.price
-    }
-    return sum
+    return cart.reduce(
+        (r, product) => r + product.price,
+        0
+    )
 }
