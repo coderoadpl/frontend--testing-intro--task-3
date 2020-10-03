a9cbfcec41bed03073192224b2b3712e8c725278
Mateusz Choma <kontakt@coderoad.pl>
2020-10-03 10:00:16 +0200

    Implementation of free shipping

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const FREE_SHIPPING_MIN_TOTAL = 200
+
 const orderTotal = (cart) => {
-    if(!Array.isArray(cart)) throw new Error('First argument must be an array!')
-    
-    return cart.reduce(
-        (total, { price, quantity }) => {
+    if (!Array.isArray(cart)) throw new Error('First argument must be an array!')
+
+    const total = cart.reduce(
+        (subTotal, { price, quantity }) => {
             const realQuantity = quantity ?? 1
-            return total + (price * realQuantity)
+            return subTotal + (price * realQuantity)
         },
         0
     )
+
+    const shipping = cart.find((product) => product.isShipping)
+    
+    if (shipping) {
+        const totalWithoutShipping = total - (shipping.price * (shipping.quantity ?? 1))
+
+        if (totalWithoutShipping > FREE_SHIPPING_MIN_TOTAL) {
+            return totalWithoutShipping
+        }
+
+        return totalWithoutShipping + shipping.price
+    }
+
+    return total
 }
