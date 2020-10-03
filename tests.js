101b4ea868e891b16cc8a6b71fbddf3e7ed8e23b
Mateusz Choma <kontakt@coderoad.pl>
2020-10-03 08:56:43 +0200

    New assertions about quantity added

--- tests.js
+++ tests.js
@@ -14,3 +14,50 @@ const cart1 = [
 if(orderTotal(cart1) !== 1000){
     console.error('Can\'t calculate total of 1 product in cart')
 }
+
+
+const cart2 = [
+    {
+        name: 'Laptop',
+        price: 1000,
+        quantity: 2,
+    }
+]
+
+if(orderTotal(cart1) !== 2000){
+    console.error('Can\'t calculate total of 1 product with 2 quantity in cart')
+}
+
+const cart3 = [
+    {
+        name: 'Laptop',
+        price: 1000,
+        quantity: 2,
+    },
+    {
+        name: 'Smartphone',
+        price: 500,
+        quantity: 1,
+    }
+]
+
+if(orderTotal(cart3) !== 2500){
+    console.error('Can\'t calculate total of 2 products with quantity in cart')
+}
+
+const cart4 = [
+    {
+        name: 'Laptop',
+        price: 1000,
+        quantity: 2,
+    },
+    {
+        name: 'Smartphone',
+        price: 500,
+        quantity: 0,
+    }
+]
+
+if(orderTotal(cart4) !== 2000){
+    console.error('Can\'t calculate total of 2 products with 0 quantity in cart')
+}
